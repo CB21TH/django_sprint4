05b2000a03750e91f5a17a5330134569a3bd54fc
Ruslan <burik.yt@mail.ru>
2026-04-23 18:47:39 +0300

    Текущее состояние перед исправлением

---
+++ blog/models.py
@@ -0,0 +1,61 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+class BaseModel(models.Model):
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    class Meta:
+        abstract = True
+
+class Location(BaseModel):
+    name = models.CharField(max_length=256, verbose_name='Название места')
+    class Meta:
+        verbose_name = 'локация'
+        verbose_name_plural = 'Локации'
+
+class Category(BaseModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(unique=True, verbose_name='Идентификатор')
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+class Post(BaseModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(verbose_name='Дата публикации')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True, verbose_name='Изображение')
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пост'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
+
+    class Meta:
+        ordering = ('created_at',)
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Комментарий {self.author} к {self.post}'
---
+++ blog/views.py
@@ -0,0 +1,141 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.core.paginator import Paginator
+from django.conf import settings
+from django.urls import reverse_lazy
+from django.utils import timezone
+
+from .models import Post, Category, Location, Comment
+from .forms import PostForm, CommentForm
+
+User = get_user_model()
+
+
+def get_published_posts():
+    """Возвращает опубликованные посты."""
+    return Post.objects.filter(
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True
+    ).select_related('author', 'location', 'category')
+
+
+def index(request):
+    post_list = get_published_posts()
+    paginator = Paginator(post_list, settings.POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/index.html', {'page_obj': page_obj})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(get_published_posts(), pk=pk)
+    comments = post.comments.all()
+    form = CommentForm()
+    return render(request, 'blog/detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+    })
+
+
+def category_posts(request, slug):
+    category = get_object_or_404(Category, slug=slug, is_published=True)
+    post_list = get_published_posts().filter(category=category)
+    paginator = Paginator(post_list, settings.POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/category.html', {
+        'category': category,
+        'page_obj': page_obj,
+    })
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    if request.user == author:
+        post_list = author.posts.all()
+    else:
+        post_list = get_published_posts().filter(author=author)
+    paginator = Paginator(post_list, settings.POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/profile.html', {
+        'author': author,
+        'page_obj': page_obj,
+    })
+
+
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+    return redirect('blog:post_detail', pk=post_id)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('blog:profile', args=[self.request.user.username])
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/create.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post_detail', args=[self.object.pk])
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/confirm_delete.html'
+    success_url = reverse_lazy('blog:index')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post_detail', args=[self.object.post.pk])
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post_detail', args=[self.object.post.pk])
